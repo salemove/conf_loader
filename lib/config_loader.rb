ebb61cce26c35810b2cd0cff5ca2a1ef6badb3ce
Indrek Juhkam <indrek@urgas.eu>
2014-02-03 18:22:07 +0200

    Simplify with symbolize gem

--- lib/config_loader.rb
+++ lib/config_loader.rb
@@ -1,4 +1,5 @@
 require 'yaml'
+require 'symbolizer'
 
 # Helps to load config files in salemove ecosystem
 #
@@ -16,21 +17,6 @@ class ConfigLoader
   # @api public
   def self.load(path, env)
     hash = YAML.load_file(path)[env]
-    normalize_hash(hash)
-  end
-
-  # Converts string keys into symbol keys
-  #
-  # @param [Hash] hash
-  #
-  # @return [Hash]
-  #   normalized hash
-  #
-  # @api private
-  def self.normalize_hash(hash)
-    hash.each_with_object({}) do |(key, value), normalized_hash|
-      normalized_value = value.is_a?(Hash) ? normalize_hash(value) : value
-      normalized_hash[key.to_sym] = normalized_value
-    end
+    Symbolizer.symbolize(hash)
   end
 end
--- spec/unit/config_loader_spec.rb
+++ spec/unit/config_loader_spec.rb
@@ -1,23 +1,4 @@
 require 'spec_helper'
 
 describe ConfigLoader do
-  describe '.normalize_hash' do
-    subject { described_class.normalize_hash(input) }
-
-    context 'when one level hash' do
-      let(:input) { {'a' => 'b'} }
-
-      it 'symbolizes keys' do
-        should eq(a: 'b')
-      end
-    end
-
-    context 'when nested hash' do
-      let(:input) { {'a' => {'b' => 'c'}} }
-
-      it 'symbolizes keys recursively' do
-        should eq(a: {b: 'c'})
-      end
-    end
-  end
 end
