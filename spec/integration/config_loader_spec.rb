5e7bdfe5cd17ee39072ddad30a5608e0ac7d7c13
chris.willmore <chris@salemove.com>
2015-09-28 12:03:25 +0300

    Expand IT to better describe deployment environment toggling

--- spec/integration/config_loader_spec.rb
+++ spec/integration/config_loader_spec.rb
@@ -3,8 +3,26 @@ require 'spec_helper'
 describe ConfigLoader do
   let(:path) { File.dirname(__FILE__) + '/../fixtures/simple.yml' }
 
-  it 'loads config with correct environment' do
-    config = described_class.load(path, 'production')
-    expect(config[:redis]).to eq(host: 'yawsah')
+  context 'config loaded with production deployment setting' do
+
+    before do
+      @config = described_class.load(path, 'production')
+    end
+
+    it 'exposes production value' do
+      expect(@config[:redis]).to eq(host: 'yawsah')
+    end
   end
+
+  context 'config loaded with development deployment setting' do
+
+    before do
+      @config = described_class.load(path, 'development')
+    end
+
+    it 'exposes development value' do
+      expect(@config[:redis]).to eq(host: 'localhost')
+    end
+  end
+
 end
