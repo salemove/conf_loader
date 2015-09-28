b1d2ac62c5c1f71195227416200ad0d5e7edbfee
chris.willmore <chris@salemove.com>
2015-09-28 13:42:30 +0300

    Replace instance variables with `let` declarations

--- spec/integration/config_loader_spec.rb
+++ spec/integration/config_loader_spec.rb
@@ -5,39 +5,36 @@ describe ConfigLoader do
 
   context 'config loaded with production deployment setting' do
 
-    before do
-      @config = described_class.load(path, 'production')
-    end
+    let(:config) { described_class.load(path, 'production') }
 
     it 'exposes production value' do
-      expect(@config[:some_service]).to eq(host: 'some_service.production.com')
+      expect(config[:some_service]).to eq(host: 'some_service.production.com')
     end
   end
 
   context 'config loaded with development deployment setting' do
 
-    before do
-      @config = described_class.load(path, 'development')
-    end
+    let(:config) { described_class.load(path, 'development') }
 
     it 'exposes development value' do
-      expect(@config[:some_service]).to eq(host: 'localhost')
+      expect(config[:some_service]).to eq(host: 'localhost')
     end
   end
 
   context 'with ENV variables declared' do
 
+    let(:config) { described_class.load(path, 'flexible') }
+
     before do
       ENV['FOO'] = 'foo-value'
-      @config = described_class.load(path, 'flexible')
     end
 
     it 'exposes ENV defined value' do
-      expect(@config[:foo]).to eq('foo-value')
+      expect(config[:foo]).to eq('foo-value')
     end
 
     it 'exposes undefined ENV values as nil' do
-      expect(@config[:undefine_val]).to be_nil
+      expect(config[:undefine_val]).to be_nil
     end
   end
 
